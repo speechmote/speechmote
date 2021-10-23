f20d2adc1d53adb47f9c6d8948a1e0caff0183fd
Rishabh Jain <r.jain.voyager@gmail.com>
2021-10-22 22:18:09 -0400

    basic contextmenu

--- src/extension/background.js
+++ src/extension/background.js
@@ -1,1 +1,12 @@
 //Background Code for Speechmote
+function getword(info,tab) {
+    console.log("Word " + info.selectionText + " was clicked.");
+    chrome.tabs.create({  
+      url: "http://www.google.com/search?q=" + info.selectionText
+    });
+  }
+  chrome.contextMenus.create({
+    title: "Search: %s", 
+    contexts:["selection"], 
+    onclick: getword
+  });
