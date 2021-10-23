7dca854daddfeee00a73a477fce296fc8e276b55
Adithya Vasudev <adithya.gvasudev@gmail.com>
2021-10-23 17:45:17 -0400

    added file endpoint

--- src/model/app.py
+++ src/model/app.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 import model
+from shutil import copyfile
+from google.cloud import storage
 
 app = FastAPI()
 
@@ -31,3 +33,32 @@ async def read_item(fileName):
 @app.get("/test/{item_id}")
 async def test_read(item_id: str):
     return {"recieved:" + item_id}
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    if file is None:
+        return "Empty"
+    upload_blob("speechmote-329915.appspot.com", file, "sample.wav")
+    phrase = model.tokenize("/code/sample.wav")
+    return phrase
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print(
+        "File {} uploaded to {}.".format(
+            source_file_name, destination_blob_name
+        )
+    )
