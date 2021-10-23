ab195e24ace305353cca16f8f24c4226f09792c1
Adithya Vasudev <adithya.gvasudev@gmail.com>
2021-10-23 13:49:40 -0400

    adding middleware

--- src/model/app.py
+++ src/model/app.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 import model
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000"
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def read_root():
