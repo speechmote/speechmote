56f973a7465bd37eff7393131054e7b3f4064297
opatil-hub <opatil31@gatech.edu>
2021-10-24 02:18:47 -0400

    Concats Blobs and Outputs as .Webm

--- src/extension/main.js
+++ src/extension/main.js
@@ -5,31 +5,46 @@ var mediaConstraints = {
 };
 
 function onMediaSuccess(stream) {
-    var mediaRecorder = new MediaStreamRecorder(stream);
+    var options = {mimeType: 'audio/webm'};
+    var mediaRecorder = new MediaRecorder(stream, options);
     var chunks = [];
-    mediaRecorder.mimeType = 'audio/wav'; // check this line for audio/wav
     mediaRecorder.ondataavailable = function (blob) {
-        chunks.push(blob);
+        chunks.push(blob.data);
         console.log(chunks.length);
-        var urlblob = URL.createObjectURL(blob);
+        /*var urlblob = URL.createObjectURL(blob);
         var a = document.createElement('a');
         document.body.appendChild(a);
         a.style = 'display: none';
         a.href = urlblob;
         a.download = 'send.wav';
         a.click();
-        console.log("wav saved to local file");
+        console.log("wav saved to local file");*/
 
     };
     mediaRecorder.start(1000);
     chrome.commands.onCommand.addListener( async function (command) {
         if (command === "stop") {
             mediaRecorder.stop();
+            download();
             console.log("data available after MediaRecorder.stop() called.");
             console.log("recorder stopped");
         }
     });
-    
+    function download() {
+        var globalBlob = new Blob(chunks, {
+            type: 'audio/webm'
+        });
+        var url = URL.createObjectURL(globalBlob);
+        console.log(globalBlob);
+        chrome.tabs.create({ url: url });
+        /*var a = document.createElement('a');
+        document.body.appendChild(a);
+        a.style = 'display: none';
+        a.href = url;
+        a.download = 'test.webm';
+        a.click();
+        window.URL.revokeObjectURL(url);*/
+    }
 }
 function onMediaError(e) {
     console.error('media error', e);
